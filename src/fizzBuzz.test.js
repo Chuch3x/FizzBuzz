86b74970ee24853a8d9d291830cac3ea64b73e96
Cesar <c.augusto_mendez@hotmail.com>
2023-03-20 16:46:48 -0400

    3er ciclo: mostrar Fizz para el numero 3

--- src/fizzBuzz.test.js
+++ src/fizzBuzz.test.js
@@ -1,4 +1,7 @@
 function fizzBuzz(num){
+  if(num == 3){
+    return "Fizz";
+  }
   return num;
 }
 
@@ -9,5 +12,7 @@ describe("FizzBuzz", () => {
   it("deberia mostrar el numero 2", () => {
     expect(fizzBuzz(2)).toEqual(2);
   });
-
+  it("deberia mostrar el numero fizz para el numero 3", () => {
+    expect(fizzBuzz(3)).toEqual("Fizz");
+  });
 });
