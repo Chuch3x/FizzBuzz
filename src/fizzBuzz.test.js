6156c5667849ade99697bd47229162cf62f77bd3
Cesar <c.augusto_mendez@hotmail.com>
2023-03-20 18:12:03 -0400

    refactorizando, moviendo las funciones a otro archivo

--- src/fizzBuzz.test.js
+++ src/fizzBuzz.test.js
@@ -1,28 +1,4 @@
-function fizzBuzz(num){
-  if(num % 3 ==0 && num % 5 == 0){
-    return "FizzBuzz";
-  }
-  else if(num % 3 == 0){
-    return "Fizz";
-  }
-  else if(num % 5 == 0){
-    return "Buzz";
-  }
-  return num;
-}
-
-function cadenaNumeros(tam){
-  let numeros = "";
-  for(let i = 0; i < tam; i++){
-    if(i == tam-1){
-      numeros = numeros + fizzBuzz(i+1);
-    }
-    else{
-      numeros = numeros + fizzBuzz(i+1) + ", ";
-    }
-  }
-  return numeros;
-}
+import { fizzBuzz, cadenaNumeros } from "./fizzBuzz";
 
 
 describe("FizzBuzz", () => {
